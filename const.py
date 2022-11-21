1485feef4a2c2e56e533c4025b1e0115266fbf80
DR <dr@alchipro.today>
2022-11-21 19:25:12 +0300

    Сделана проверка try

--- const.py
+++ const.py
@@ -2,4 +2,8 @@ def msg(text=''):
     # вывод сообщения
     print(text)
 
+def error_msg(text=''):
+    print('ОШИБКА!')
+    print(text)
+
 EXIT_CODE = '0'
--- fnew.py
+++ fnew.py
@@ -1,4 +1,21 @@
 from const import *
 
 def new():
-    msg('Новый файл...')
+    try:
+        n = 100*0
+
+        try:
+            n = 100**10
+        except:
+            error_msg('Многовато будет')
+        else:
+            msg('Обошлось')
+
+    except ZeroDivisionError:
+        error_msg('Деление на ноль!')
+    except AttributeError:
+        error_msg('Ошибка атрибута!')
+    else:
+        msg(n)
+    finally:
+        msg('Всё завершилось')
